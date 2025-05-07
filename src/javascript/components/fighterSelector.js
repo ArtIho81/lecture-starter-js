6156c70fe458d28eb2c40432b3018ff40881ae75
Ihor Artomov <igora2406@gmail.com>
2025-05-07 14:52:35 +0300

    getFighterInfo is  implemented

--- src/javascript/components/fighterSelector.js
+++ src/javascript/components/fighterSelector.js
@@ -7,6 +7,11 @@ const fighterDetailsMap = new Map();
 
 export async function getFighterInfo(fighterId) {
     // get fighter info from fighterDetailsMap or from service and write it to fighterDetailsMap
+    if (!fighterDetailsMap.has(fighterId)) {
+        const fighter = await fighterService.getFighterDetails(fighterId);
+        fighterDetailsMap.set(fighterId, fighter);
+    }
+    return fighterDetailsMap.get(fighterId);
 }
 
 function startFight(selectedFighters) {
