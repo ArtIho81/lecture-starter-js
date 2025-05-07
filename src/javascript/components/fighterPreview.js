697a769c29669070213024bb8c1033396cb1c6a6
Ihor Artomov <igora2406@gmail.com>
2025-05-07 15:57:03 +0300

    + fighterPreview is added

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -1,17 +1,5 @@
 import createElement from '../helpers/domHelper';
 
-export function createFighterPreview(fighter, position) {
-    const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
-    const fighterElement = createElement({
-        tagName: 'div',
-        className: `fighter-preview___root ${positionClassName}`
-    });
-
-    // todo: show fighter info (image, name, health, etc.)
-
-    return fighterElement;
-}
-
 export function createFighterImage(fighter) {
     const { source, name } = fighter;
     const attributes = {
@@ -26,4 +14,44 @@ export function createFighterImage(fighter) {
     });
 
     return imgElement;
+}
+
+export function createFighterPreview(fighter, position) {
+    const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
+    const fighterElement = createElement({
+        tagName: 'div',
+        className: `fighter-preview___root ${positionClassName}`
+    });
+
+    // todo: show fighter info (image, name, health, etc.)
+    if (fighter) {
+        const { name, source } = fighter;
+        const nameElement = createElement({
+            tagName: 'h2',
+            className: 'fighter-preview__name'
+        });
+        nameElement.innerHTML = `${name}`;
+        const infoElement = createElement({
+            tagName: 'div',
+            className: 'fighter-preview___info'
+        });
+        Object.entries(fighter).map(prop => {
+            const [key, value] = prop;
+            if (key === 'health' || key === 'attack' || key === 'defense') {
+                const data = createElement({
+                    tagName: 'p',
+                    className: 'fighter-preview__prop'
+                });
+                data.innerHTML = `${key}: ${value}`;
+                infoElement.appendChild(data);
+            }
+            return null;
+        });
+        const imgElement = createFighterImage({ name, source });
+        fighterElement.appendChild(nameElement);
+        fighterElement.appendChild(imgElement);
+        fighterElement.appendChild(infoElement);
+    }
+
+    return fighterElement;
 }
